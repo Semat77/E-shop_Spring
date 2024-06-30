0600c06356d812f2df1ab1a1417d5871693a61a5
Sebastian Matuszczyk <s.matuszczyk1@gmail.com>
2024-06-30 23:42:01 +0200

    Initial Commit

---
+++ src/main/java/pl/eshoping/EShopingApplication.java
@@ -0,0 +1,13 @@
+package pl.eshoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShopingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EShopingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/eshoping/EShopingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.eshoping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
