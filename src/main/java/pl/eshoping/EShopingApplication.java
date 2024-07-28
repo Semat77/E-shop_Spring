c571609078371a653ba0cf9ef21fbcae10505fe4
Sebastian Matuszczyk <s.matuszczyk1@gmail.com>
2024-07-28 23:50:47 +0200

    application configure

--- src/main/java/pl/eshoping/EShopingApplication.java
+++ src/main/java/pl/eshoping/EShopingApplication.java
@@ -2,8 +2,12 @@ package pl.eshoping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"pl.eshoping.Model"})
+@EnableJpaRepositories(basePackages = {"pl.eshoping.Repository"})
 public class EShopingApplication {
 
     public static void main(String[] args) {
